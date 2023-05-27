bc617bc3eccc544a99256b4e726a09dfa52e0c2b
Angel Malewar <112496963+AngelMalewar@users.noreply.github.com>
2023-05-27 17:53:27 +0530

    Update server.js
    Updated

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const configuration = new Configuration({
+    organization: process.env.OPENAI_API_ORG,
     apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -29,19 +30,48 @@ app.post('/', async (req, res) => {
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 3000,
+            max_tokens: 4000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
         });
+
+        // Remove specific lines and blocks of code from the response
+        let botResponse = response.data.choices[0].text;
+        botResponse = botResponse.replace("package com.example.demo.controller;", "")
+            .replace("import java.util.List;", "")
+            .replace("import org.springframework.beans.factory.annotation.Autowired;", "")
+            .replace("import org.springframework.web.bind.annotation.*;", "")
+            .replace("import com.example.demo.model.*;", "")
+            .replace("import com.example.demo.*;", "")
+            .replace("@RestController", "")
+            .replace("@RequestMapping(path=\"/demo\")", "")
+            .replace("public class MainController {", "")
+            .replace("@Autowired", "")
+            .replace("private UserRepository userRepository;", "")
+            .replace("@GetMapping(path=\"/add\")", "")
+            .replace("public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {", "")
+            .replace("User n = new User();", "")
+            .replace("n.setName(name);", "")
+            .replace("n.setEmail(email);", "")
+            .replace("userRepository.save(n);", "")
+            .replace("return \"Saved\";", "")
+            .replace("@GetMapping(path=\"/all\")", "")
+            .replace("public @ResponseBody Iterable<User> getAllUsers() {", "")
+            .replace("public @ResponseBody Iterable<User> getAllUsers() {", "")
+            .replace("return userRepository", "") + "  ";
+
+        // Remove remaining comments
+        botResponse = botResponse.replace(/\/\/.*(?:\r\n|\r|\n|$)/g, "");
+
         res.status(200).send({
-            bot: response.data.choices[0].text
-        })
+            bot: botResponse
+        });
     }
     catch (error) {
         console.log(error);
-        res.status(500).send({ error })
+        res.status(500).send({ error });
     }
-})
+});
 
-app.listen(5000, () => console.log("Sever is running on port :- http://localhost:5000"))
+app.listen(5000, () => console.log("Server is running on port: http://localhost:5000"));
